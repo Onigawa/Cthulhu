fc09b4ac9e22c6bb7ef2a73e8fadc3f2cb91675a
Onigawa <eric.hantala@wanadoo.fr>
2018-05-25 09:00:59 +0200

    V1.0

--- app.R
+++ app.R
@@ -1,5 +1,7 @@
 ## app.R ##
-library(shinydashboard)
+require(shinydashboard)
+require(stringr)
+require(DT)
 #staplr for pdf form fill up (must install some software so not shiny)
 
 rollDice<-function(dice=6,nbr=1,sum=F){
@@ -8,16 +10,19 @@ rollDice<-function(dice=6,nbr=1,sum=F){
   return(res)
 }
 
-occupation<-read.csv2(file = "Occupation.csv")
-
-
+occupation<-read.csv2(file = "Occupation.csv",encoding = "ANSI") #HERE
+names<-read.csv2(file = "Names.csv",encoding = "ANSI")
+attache<-read.csv2(file = "Attaches.csv",encoding = "ANSI")
+armes<-read.csv2(file = "Armes.csv",encoding = "ANSI")
 ui <- dashboardPage(
   dashboardHeader(title = "Character Generator"),
   ## Sidebar content
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Stats", tabName = "Stats", icon = icon("dashboard")),
-      menuItem("Job", tabName = "Job", icon = icon("th"))
+      menuItem("Stats", tabName = "Stats", icon = icon("calculator")),
+      menuItem("Job", tabName = "Job", icon = icon("user-md")),
+      menuItem("Personal", tabName = "Personal", icon = icon("user")),
+      menuItem("Equipment", tabName = "Equipment", icon = icon("bomb"))
     )
   ),
   ## Body content
@@ -70,24 +75,96 @@ ui <- dashboardPage(
       tabItem(tabName = "Job",
               box(title = "Points",width = 12,
                   fluidRow(
+                    column(width = 3, checkboxInput("Recommanded","Show Only Recommanded",value = F)),
                     column(width = 6,selectInput(inputId = "Occupation",label = "Occupation",choices = occupation$Occupation)),
-                    column(width = 6,numericInput(inputId = "Credit",label = "Credit",value = 10,min = 0,max = 90)),
                     column(width = 6,htmlOutput(outputId = "InterestPoint")),
                     column(width = 6,htmlOutput(outputId = "OccupationPoint"))
                   )
               ),
-              box(title = "Credit",width=12,
+              box(title = "Credit",width=6,
                   fluidRow(
                     column(width = 6,selectInput(inputId = "Period",label = "Period",choices = c("Classic","Modern"))),
-                    column(width = 6,tableOutput("CreditTable"))
+                    column(width = 6,numericInput(inputId = "Credit",label = "Credit",value = 10,min = 0,max = 90)),
+                    column(width = 12,tableOutput("CreditTable"))
+                  )
+              ),
+              box(title = "Skills",width=6,
+                  tableOutput(outputId = "SkillTable")
+              )
+              
+      ),
+      
+      tabItem(tabName="Personal",
+               box(title = "Name",width = 12,
+                   fluidRow(
+                     column(width = 4,selectInput(inputId = "Gender",label = "Prefered Gender",choices = c("Male","Female","Random"))),
+                     column(width = 4,textInput(inputId = "FirstName",label = "First Name",placeholder = "Jane")),
+                     column(width = 4,textInput(inputId = "LastName",label = "Last Name",placeholder = "Smith")),
+                     column(width = 3,actionButton(inputId = "RollName",label = "Random Name"))
+                   )
+                   ),
+              box(title = "Particular",width = 12,
+                  fluidRow(
+                    column(width = 4,textInput(inputId = "Croyance",label = "Believes",placeholder = "God")),
+                    column(width = 4,textInput(inputId = "Personne",label = "Important person",placeholder = "Jane Smith")),
+                    column(width = 4,textInput(inputId = "PersonReason",label = "Why person important",placeholder = "Sister")),
+                    column(width = 4,textInput(inputId = "Place",label = "Important place",placeholder = "Home")),
+                    column(width = 4,textInput(inputId = "Object",label = "Important object",placeholder ="Necklace")),
+                    column(width = 4,textInput(inputId = "Characteristic",label = "Characteristic",placeholder="Happy")),
+                    column(width = 1,actionButton(inputId = "RandomCharacteristics",label = "Randomize"))
                   )
               )
-      )
+               
+               ),
+      tabItem(tabName = "Equipment",
+              
+                box(title = "Weapons",width=12,
+                    fluidRow(
+                      column(width = 6, selectInput(inputId = "WeaponPeriod",label = "Period",choices = c("all","1890s", "1920s","present","rare"),selected = "all")),
+                      column(width = 6, selectInput(inputId = "WeaponCategory",label = "Period",choices =unique(armes$Category))),
+                      column(width = 12,DTOutput(outputId = "WeaponTable")))
+                      )
+              )
     )
   )
 )
 
 server <- function(input, output,session) {
+  
+  output$WeaponTable<-renderDT({
+    
+    temp<-armes[armes$Category==input$WeaponCategory,]
+    res<-temp[sapply(X =temp$Era,FUN = grepl,pattern=input$WeaponPeriod ),]
+    res<-rbind(res,temp[temp$Era=="all",])
+    res[!duplicated(res),]
+  })
+  
+  observeEvent(eventExpr = input$RandomCharacteristics,{
+    dice<-rollDice(dice =10,nbr = 6 )
+    updateTextInput(session=session,inputId = "Croyance",label = "Believes",value = attache[dice[1],"Croyance"])
+    updateTextInput(session=session,inputId = "Personne",label = "Important person",value = attache[dice[2],"Personne"])
+    updateTextInput(session=session,inputId = "PersonReason",label = "Why person important",value = attache[dice[3],"Raison"])
+    updateTextInput(session=session,inputId = "Place",label = "Important place",value = attache[dice[4],"Lieux"])
+    updateTextInput(session=session,inputId = "Object",label = "Important object",value =attache[dice[5],"Bien"])
+    updateTextInput(session=session,inputId = "Characteristic",label = "Characteristic",value=attache[dice[6],"Traits"])
+  })
+  
+  observeEvent(eventExpr = input$RollName,{
+    dice<-rollDice(dice = 100,nbr = 2)
+    if(input$Gender == "Male"){
+      updateTextInput(session=session,inputId = "FirstName",label = "First Name",value = names[dice[1],"Masculin"])
+    }else if(input$Gender == "Female"){
+      updateTextInput(session=session,inputId = "FirstName",label = "First Name",value = names[dice[1],"Feminin"])
+    }else{
+      if(rollDice(dice = 2,nbr = 1)==1){
+        updateTextInput(session=session,inputId = "FirstName",label = "First Name",value = names[dice[1],"Feminin"])
+      }else{
+        updateTextInput(session=session,inputId = "FirstName",label = "First Name",value = names[dice[1],"Masculin"])
+      }
+    }
+
+    updateTextInput(session=session,inputId = "LastName",label = "Last Name",value =names[dice[2],"Famille"])
+  })
   
   observeEvent(eventExpr = input$reroll,{
     updateTextInput(session = session,inputId = "DiceFOR",value =as.character(rollDice(nbr = 3,dice = 6,sum = T)) )
@@ -111,7 +188,7 @@ server <- function(input, output,session) {
     Stats["FOR"]<-Stats["FOR"]-input$ageFOR
     Stats["DEX"]<-Stats["DEX"]-input$ageDEX
     Stats["CON"]<-Stats["CON"]-input$ageCON
-    Stats["EDU"]<-Stats["EDU"]-input$ageEDU
+    Stats["EDU"]<-Stats["EDU"]+input$ageEDU
     res<-data.frame(Stats=Stats,Half=floor(Stats/2),Fifth=floor(Stats/5))
     rownames(res)<-c("FOR","CON","DEX","TAI","INT","EDU","APP","POU","Chance")
     res
@@ -173,23 +250,24 @@ server <- function(input, output,session) {
     paste("<b>Interest Points </b> :",floor(as.integer(input$DiceINT)*5)*2)
   })
   output$OccupationPoint<-renderText({
-    points<-(floor(as.integer(input$DiceEDU)*5)-input$ageEDU)*2
+    points<-(floor(as.integer(input$DiceEDU)*5)+input$ageEDU)*2
     job<-occupation[occupation$Occupation==input$Occupation,]
     JobCategory<-as.character(job$Skill1)
+    appAgeMod<-ceiling(as.numeric(as.character(cut(as.numeric(input$Age), breaks=c(0, 25, 40, 50, 60, 70,80,750),labels = c(0,-0.1,-5,-10,-15,-20,-25)))))
     switch (JobCategory,
-            "EDU" = points<-points+2*(floor(as.integer(input$DiceEDU)*5)-input$ageEDU),
+            "EDU" = points<-points+2*(floor(as.integer(input$DiceEDU)*5)+input$ageEDU),
             "FOR" = points<-points+2*(floor(as.integer(input$DiceFOR)*5)-input$ageFOR),
             "INT" = points<-points+2*(floor(as.integer(input$DiceINT)*5)),
             "POU" = points<-points+2*(floor(as.integer(input$DicePOU)*5)),
-            "APP" = points<-points+2*(floor(as.integer(input$DiceAPP)*5)-input$ageAPP),
+            "APP" = points<-points+2*(floor(as.integer(input$DiceAPP)*5)+appAgeMod),
             "DEX" = points<-points+2*(floor(as.integer(input$DiceDEX)*5)-input$ageDEX)
     )
     points1<-points
     JobCategory<-as.character(job$Skill2)
-    points<-(floor(as.integer(input$DiceEDU)*5)-input$ageEDU)*2
+    points<-(floor(as.integer(input$DiceEDU)*5)+input$ageEDU)*2
     if(!is.na(JobCategory)){
       switch (JobCategory,
-              "EDU" = points<-points+2*(floor(as.integer(input$DiceEDU)*5)-input$ageEDU),
+              "EDU" = points<-points+2*(floor(as.integer(input$DiceEDU)*5)+input$ageEDU),
               "FOR" = points<-points+2*(floor(as.integer(input$DiceFOR)*5)-input$ageFOR),
               "INT" = points<-points+2*(floor(as.integer(input$DiceINT)*5)),
               "POU" = points<-points+2*(floor(as.integer(input$DicePOU)*5)),
@@ -261,6 +339,41 @@ server <- function(input, output,session) {
     res<-data.frame(Current=cur,Cash=esp,Capital=cap)
     res
   },digits = 1)
+  
+  output$SkillTable<-renderTable(expr = {str_to_title(strsplit(as.character(occupation[occupation$Occupation==input$Occupation,"Skills"]),", ")[[1]])},colnames = F)
+  
+  observeEvent(input$Recommanded,{
+    if(input$Recommanded){
+      updateSelectInput(session=session,inputId = "Occupation",label = "Occupation",choices = isolate(Recommanded())$Occupation)
+    }else{
+      updateSelectInput(session=session,inputId = "Occupation",label = "Occupation",choices = occupation$Occupation)
+    }
+  })
+
+  Recommanded<-reactive({
+    temp<-NULL
+      FOR<-(floor(as.integer(input$DiceFOR)*5)-input$ageFOR)
+      DEX<-(floor(as.integer(input$DiceDEX)*5)-input$ageDEX)
+      EDU<-(floor(as.integer(input$DiceEDU)*5)+input$ageEDU)
+      POU<-(floor(as.integer(input$DicePOU)*5))
+      appAgeMod<-ceiling(as.numeric(as.character(cut(as.numeric(input$Age), breaks=c(0, 25, 40, 50, 60, 70,80,750),labels = c(0,-0.1,-5,-10,-15,-20,-25)))))
+      APP<-(floor(as.integer(input$DiceAPP)*5)+appAgeMod)
+      
+      maxs<-which( c(FOR,DEX,EDU,POU,APP) == max(c(FOR,DEX,EDU,POU,APP)) )
+      if(1 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill1=="FOR",])
+      if(1 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill2=="FOR",])  
+      if(2 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill1=="DEX",])
+      if(2 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill2=="DEX",])
+      if(3 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill1=="EDU",])
+      if(3 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill2=="EDU",])
+      if(4 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill1=="POU",])
+      if(4 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill2=="POU",])
+      if(5 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill1=="APP",])
+      if(5 %in% maxs) temp<-rbind(temp,occupation[occupation$Skill2=="APP",])
+      temp[!is.na(temp$Occupation),]
+    
+  })
+  
   observeEvent(eventExpr = input$DEBUG,handlerExpr = browser())
 }
 
